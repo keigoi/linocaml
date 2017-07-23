94946957b371b601ead05fe61f72162d4115e31c
Keigo IMAI <keigo.imai@gmail.com>
2017-07-23 17:37:16 +0900

    simplify

--- examples/linlist.ml
+++ examples/linlist.ml
@@ -21,8 +21,7 @@ let rec map f =
   match%lin get s with
   | Cons(x, #s) ->
     map f >>
-    let%lin #s = [%linval Cons(Data_Internal__ (f x), !!s)]
-    in return ()
+    put s [%linval Cons(Data_Internal__ (f x), !!s)]
   | Nil -> putval s Nil
 
 (* equivalent to List.rev_map (though this one is non tail-recursive) *)
@@ -30,7 +29,7 @@ let rev_map f =
   let rec loop () =
     match%lin get s with
     | Cons(x, #s) ->
-       let%lin #t = [%linval Cons(Data_Internal__ (f x), !!t)] in
+       put t [%linval Cons(Data_Internal__ (f x), !!t)] >>
        loop ()
     | Nil -> return ()
   in
@@ -38,15 +37,13 @@ let rev_map f =
   putval t Nil >>
   loop () >>
   (* put it back to s *)
-  let%lin #s = get t in
-  return ()
+  put s (get t)
 
 (* turn a normal list into linear list *)
 let rec make = function
   | x::xs ->
      make xs >>
-     let%lin #s = [%linval Cons(Data_Internal__ x, !!s)]
-     in return ()
+     put s [%linval Cons(Data_Internal__ x, !!s)]
   | [] ->
      putval s Nil
 
@@ -56,8 +53,7 @@ let () =
     make [1;2;3] >>
     map (fun x -> x+1) >>
     map string_of_int >>
-    iter (fun x -> print_endline x; return ()) >>
-    return ()
+    iter (fun x -> print_endline x; return ())
   in
   run_ctx f ()
 
@@ -74,8 +70,7 @@ let rec map f s1 s2 =
   match%lin get s1 with
   | Cons(x, #s2) ->
      map f s1 s2 >>
-     let%lin #s2 = [%linval Cons(Data_Internal__ (f x), !! s1)]
-     in return ()
+     put s2 [%linval Cons(Data_Internal__ (f x), !! s1)]
   | Nil -> putval s2 Nil
 
 (* more liberal one *)
@@ -83,8 +78,7 @@ let rec map f s1 s2 s3 s4 =
   match%lin get s1 with
   | Cons(x, #s2) ->
      map f s1 s2 s3 s4 >>
-     let%lin #s4 = [%linval Cons(Data_Internal__ (f x), !! s3)]
-     in return ()
+     put s4 [%linval Cons(Data_Internal__ (f x), !! s3)]
   | Nil -> putval s4 Nil
 
 module O = struct
@@ -96,42 +90,34 @@ let root = {Lens.get=(fun x -> x); Lens.put=(fun _ x -> x)}
 
 (* with less slot parameters, by using "temporary environment" *)
 let rec map f s1 s2 =
-  let%lin #root = [%linval object method tmp=Empty method outer= !!root end]
-  in
   let s1' = s1 ##. O.outer
   and s2' = s2 ##. O.outer
-  in
-  let%lin #O.tmp = get s1'
   in
   let rec loop () =
     match%lin get O.tmp with
     | Cons(x, #O.tmp) ->
        loop () >>
-       let%lin #O.tmp = [%linval Cons(Data_Internal__ (f x), !! O.tmp)]
-       in return ()
+       put O.tmp [%linval Cons(Data_Internal__ (f x), !! O.tmp)]
     | Nil -> putval O.tmp Nil
   in
+  put root [%linval object method tmp=Empty method outer= !!root end] >>
+  put O.tmp (get s1') >>
   loop () >>
-  let%lin #s2' = get O.tmp
-  in
-  let%lin #root = O.linval_t (get O.outer) in
-  return ()
+  put s2' (get O.tmp) >>
+  put root (O.linval_t (get O.outer))
 
 let iter f s =
-  let%lin #root = [%linval object method tmp=Empty method outer= !!root end]
-  in
   let s' = s ##. O.outer
-  in
-  let%lin #O.tmp = get s'
   in
   let rec loop () =
     match%lin get O.tmp with
     | Cons(x, #O.tmp) -> f x; loop ()
     | Nil -> return ()
   in
+  put root [%linval object method tmp=Empty method outer= !!root end] >>
+  put O.tmp (get s') >>
   loop () >>
-  let%lin #root = O.linval_t (get O.outer) in
-  return ()
+  put root (O.linval_t (get O.outer))
            
   
 (* again play wth them *)
@@ -140,55 +126,42 @@ let () =
     make [1;2;3] >>
     map (fun x -> x+1) s s >>
     map string_of_int s s >>
-    iter (fun x -> print_endline x; return ()) s >>
-    return ()
+    iter (fun x -> print_endline x; return ()) s
   in
   run_ctx f ()
 
 (* with less slot parameters, by using "temporary environment" *)
-let map f s = lin_split begin
-  let%lin #root = [%linval object method tmp=Empty method outer= !!root end]
-  in
-  let s' = s ##. O.outer
-  in
-  let%lin #O.tmp = get s'
-  in
-  let rec loop () =
-    match%lin get O.tmp with
-    | Cons(x, #O.tmp) ->
-       loop () >>
-       let%lin #O.tmp = [%linval Cons(Data_Internal__ (f x), !! O.tmp)]
-       in return ()
-    | Nil -> putval O.tmp Nil
-  in
-  loop () >>
-  let%lin #root = O.linval_t [%linval (!! O.outer, !! O.tmp) ] in
-  return () 
-end
-
+let map f s =
+  lin_split begin
+    let s' = s ##. O.outer
+    in
+    let rec loop () =
+      match%lin get O.tmp with
+      | Cons(x, #O.tmp) ->
+         loop () >>
+         put O.tmp [%linval Cons(Data_Internal__ (f x), !! O.tmp)]
+      | Nil -> putval O.tmp Nil
+    in
+    put root [%linval object method tmp=Empty method outer= !!root end] >>
+    put O.tmp (get s') >>
+    loop () >>
+    put root (O.linval_t [%linval (!! O.outer, !! O.tmp) ])
+  end
+  
 (* again play wth them *)
 let () =
   let f () =
     make [100;200;300] >>
-    let%lin #s = map (fun x -> x*2) s in
-    let%lin #s = map string_of_int s in
-    iter (fun x -> print_endline x; return ()) s >>
-    return ()
+    put s (map (fun x -> x*2) s) >>
+    put s (map string_of_int s) >>
+    iter (fun x -> print_endline x; return ()) s
   in
   run_ctx f ()
    
 let f () =
-  let%lin #s = [%linval object method tmp=Empty method outer= !!s end] in
-  let%lin #s = [%linval object method tmp=Empty method outer= !!s end] in
-  return ()
-
-let f () =
-  let%lin #root = [%linval object method tmp=Empty method outer= !!root end] in
   let s = s ##. O.outer
   in
-  (*generated part end*)
+  put root [%linval object method tmp=Empty method outer= !!root end] >>
   let%lin `A(#O.tmp) = get s in
-  let%lin #s = get O.tmp in
-  let%lin #root = O.linval_t (get O.outer)
-  in
-  return ()
+  put s (get O.tmp) >>
+  put root (O.linval_t (get O.outer))
